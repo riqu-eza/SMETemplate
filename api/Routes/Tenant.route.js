079159976ec270e0cebdc14414fedb5c5274cc33
offline <vickymuthunga@gmail.com>
2025-03-13 15:44:41 +0300

    well off

--- api/Routes/Tenant.route.js
+++ api/Routes/Tenant.route.js
@@ -1,8 +1,13 @@
 import express from 'express';
 const router = express.Router();
+import dotenv from "dotenv";
 
 // Route to fetch metadata for a given domain
 router.get('/:domain/metadata', (req, res) => {
+
+  dotenv.config();
+  
+
   const { domain } = req.params;
   const tenantConfigs = process.env.TENANT_CONFIGS ? JSON.parse(process.env.TENANT_CONFIGS) : {};
 
